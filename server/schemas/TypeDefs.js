922a428f8d2ec53286b13b8e83d474ba7f99ba59
jdog <thuz3rfr13ndly@gmail.com>
2022-09-23 20:10:39 -0600

    update Typedefs

---
+++ server/schemas/TypeDefs.js
@@ -0,0 +1,24 @@
+//define the data types of the models for the schema
+
+//import graphql from apollo server
+import { gql } from 'apollo-server-express'
+
+const typeDefs = gql`
+    type Book {
+
+    }
+
+    type User {
+
+    }
+
+    type Query {
+
+
+    }
+
+    type Mutation {
+
+
+    }
+`
