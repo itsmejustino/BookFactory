7250ab00b74ca1c0ac57355a4b53599cc2aa9f0c
jdog <thuz3rfr13ndly@gmail.com>
2022-09-26 16:13:36 -0600

    add to typedefs

--- server/schemas/TypeDefs.js
+++ server/schemas/TypeDefs.js
@@ -5,15 +5,24 @@ import { gql } from 'apollo-server-express'
 
 const typeDefs = gql`
     type Book {
-
+        _id: ID
+        authors: [String]
+        description: !String,
+        bookId: !String,
+        image: String,
+        link: String,
+        title: !String
     }
 
     type User {
-
+        username: !String,
+        email: !String,
+        password: !String,
+        savedBooks: [Book]
     }
 
     type Query {
-
+        getSingleUser()
 
     }
 
